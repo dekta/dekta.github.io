0b21fd1f98c58b2724c9996862ab11c5de7b6aff
dekta <ektadhawal6306@gmail.com>
2023-01-10 02:09:20 +0530

    portfolio update

--- index.js
+++ index.js
@@ -18,13 +18,22 @@ let scroll  = document.querySelector('.scroll-up-btn')
              scroll.classList.remove("show");
         }
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        $('html').css("scrollBehavior", "auto");
-    });
+        $('.scroll-up-btn').click(function(){
+                    $('html').animate({scrollTop: 0});
+                    $('html').css("scrollBehavior", "auto");
+                });
 
 
  }
+
+ $('#nav-menu .menu li a').click(function(){
+            $('html').css("scrollBehavior", "smooth");
+        });
+
+ $('.menu-btn').click(function(){
+            $('#nav-menu .menu').toggleClass("active");
+            $('.menu-btn i').toggleClass("active");
+        });
 
  var typed = new Typed(".typing", {
     strings: [ "Back-End Developer"],
@@ -65,3 +74,75 @@ $('.carousel').owlCarousel({
 
 
 
+//     $(window).scroll(function(){
+//         // sticky navbar on scroll script
+//         if(this.scrollY > 20){
+//             $('#nav-menu').addClass("sticky");
+//         }else{
+//             $('#nav-menu').removeClass("sticky");
+//         }
+        
+//         // scroll-up button show/hide script
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
+
+//     // slide-up script
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         // removing smooth scroll on slide-up button click
+//         $('html').css("scrollBehavior", "auto");
+//     });
+
+//     $('#nav-menu .menu li a').click(function(){
+//         // applying again smooth scroll on menu items click
+//         $('html').css("scrollBehavior", "smooth");
+//     });
+
+//     // toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('#nav-menu .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+
+//     // typing text animation script
+//     var typed = new Typed(".typing", {
+//         strings: [ "Back-End Developer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     var typed = new Typed(".typing-2", {
+//         strings: [" Back-End Developer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     // owl carousel script
+//     $('.carousel').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         autoplay: true,
+//         autoplayTimeOut: 2000,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0:{
+//                 items: 1,
+//                 nav: false
+//             },
+//             600:{
+//                 items: 2,
+//                 nav: false
+//             },
+//             1000:{
+//                 items: 3,
+//                 nav: false
+//             }
+//         }
+//     });
+// });
