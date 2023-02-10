3a7199b988e3614a67e1632f652d4434326d6190
dekta <ektadhawal6306@gmail.com>
2023-02-10 12:17:55 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-
-
-let nav = document.querySelector('#nav-menu');
-let scroll  = document.querySelector('.scroll-up-btn')
-
- changenavBack();
-
- window.addEventListener('scroll', changenavBack);
- function changenavBack()
- {
-     if (window.scrollY >= 25) {
-         nav.classList.add('sticky');
-     } else {
-         nav.classList.remove('sticky');
-     }
-
-     if(window.scrollY > 500){
-            scroll.classList.add("show");
-    }else{
-             scroll.classList.remove("show");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('#nav-menu').addClass("sticky");
+        }else{
+            $('#nav-menu').removeClass("sticky");
         }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-        $('.scroll-up-btn').click(function(){
-                    $('html').animate({scrollTop: 0});
-                    $('html').css("scrollBehavior", "auto");
-                });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
+    $('#nav-menu .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
- }
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('#nav-menu .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
- $('#nav-menu .menu li a').click(function(){
-            $('html').css("scrollBehavior", "smooth");
-        });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Back-End Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
- $('.menu-btn').click(function(){
-            $('#nav-menu .menu').toggleClass("active");
-            $('.menu-btn i').toggleClass("active");
-        });
+    var typed = new Typed(".typing-2", {
+        strings: [" Back-End Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
- var typed = new Typed(".typing", {
-    strings: [ "Back-End Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-var typed = new Typed(".typing-2", {
-    strings: [" Back-End Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-
-$('.carousel').owlCarousel({
-            margin: 20,
-            loop: true,
-            autoplay: true,
-            autoplayTimeOut: 2000,
-            autoplayHoverPause: true,
-            responsive: {
-                0:{
-                    items: 1,
-                    nav: false
-                },
-                769:{
-                    items: 2,
-                    nav: false
-                },
-                1014:{
-                    items: 3,
-                    nav: false
-                }
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
             }
-        });
-
+        }
+    });
+});
